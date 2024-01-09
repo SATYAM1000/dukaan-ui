f5d44ec4a337a268b235b360dd98532cb6487e85
Satyam Kumar <satyamsingh748846@gmail.com>
2024-01-09 21:25:56 +0530

    change

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,11 +1,12 @@
 /** @format */
 
 import "./Page.css";
+import home from '../assets/home.png'
 
 const PageName = ({ item }) => {
 	return (
 		<div className="page-name">
-			<img src={item.path} className="icon-home" alt="" />
+			<img src={home} className="icon-home" alt="" />
 			<div className="page-name-box">
 				<p className="page-name-home">{item.name}</p>
 			</div>
